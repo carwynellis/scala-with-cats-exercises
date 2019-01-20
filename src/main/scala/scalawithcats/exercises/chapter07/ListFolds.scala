02ef23f0f8a39a79758675ec831b6da8fcbb7af6
Carwyn Ellis <carwynellis@gmail.com>
2019-01-20 18:31:27 +0000

    Exercise 7.1.3 solution

--- src/main/scala/scalawithcats/exercises/chapter07/ListFolds.scala
+++ src/main/scala/scalawithcats/exercises/chapter07/ListFolds.scala
@@ -1,9 +1,22 @@
 package scalawithcats.exercises.chapter07
 
+import cats.Monoid
+
 object ListFolds {
 
-  def foldListLeft[A](l: List[A]) = l.foldLeft(List.empty[A]){ (acc, elem) => elem :: acc }
+  def foldListLeft[A](l: List[A]) = l.foldLeft(List.empty[A]) { (acc, elem) => elem :: acc }
 
-  def foldListRight[A](l: List[A]) = l.foldRight(List.empty[A]){ (elem, acc) => elem :: acc }
+  def foldListRight[A](l: List[A]) = l.foldRight(List.empty[A]) { (elem, acc) => elem :: acc }
+
+  def map[A, B](l: List[A])(f: A => B) = l.foldRight(List.empty[B]) { (elem, acc) => f(elem) :: acc }
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]) = l.foldRight(List.empty[B]) { (elem, acc) => f(elem) ::: acc }
+
+  def filter[A](l: List[A])(p: A => Boolean) = l.foldRight(List.empty[A]) { (elem, acc) =>
+    if (p(elem)) elem :: acc
+    else acc
+  }
+
+  def sum[A](l: List[A])(implicit m: Monoid[A]) = l.foldRight(m.empty) { (elem, acc) => m.combine(elem, acc) }
 
 }
--- src/test/scala/scalawithcats/exercises/chapter07/ListFoldsSpec.scala
+++ src/test/scala/scalawithcats/exercises/chapter07/ListFoldsSpec.scala
@@ -16,4 +16,29 @@ class ListFoldsSpec extends FlatSpec with Matchers {
     ListFolds.foldListRight(List(1, 2, 3)) shouldBe List(1, 2, 3)
   }
 
+  behavior of "map"
+
+  it should "apply function to each element of the list preserving original list order" in {
+    ListFolds.map(List(1, 2, 3))(_ + 1) shouldBe List(2, 3, 4)
+  }
+
+  behavior of "flatMap"
+
+  it should "apply function to each element of the list preserving original list order" in {
+    ListFolds.flatMap(List(1, 2, 3))(i => List(i + 1)) shouldBe List(2, 3, 4)
+  }
+
+  behavior of "filter"
+
+  it should "retain elements of the list that satisfy the supplied predicate" in {
+    ListFolds.filter(List(1, 2, 3))(_ % 2 == 1) shouldBe List(1, 3)
+  }
+
+  behavior of "sum"
+
+  it should "return the sum of the list" in {
+    import cats.instances.int._
+    ListFolds.sum(List(1, 2, 3)) shouldBe 6
+  }
+
 }
