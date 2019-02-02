840de90217e97bb57029fdf3a6ee432a896274eb
Carwyn Ellis <carwynellis@gmail.com>
2019-02-02 18:02:28 +0000

    Exercise 9.3 solution

--- src/main/scala/scalawithcats/exercises/chapter09/FoldMap.scala
+++ src/main/scala/scalawithcats/exercises/chapter09/FoldMap.scala
@@ -1,10 +1,20 @@
 package scalawithcats.exercises.chapter09
 
 import cats.Monoid
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object FoldMap {
 
   def foldMap[A,B : Monoid](v: Vector[A])(f: A => B): B =
     v.map(f).fold(Monoid[B].empty)(Monoid[B].combine)
+
+  private val Processors = Runtime.getRuntime.availableProcessors()
+
+  def parallelFoldMap[A, B : Monoid](values: Vector[A])(func: A => B): Future[B] = {
+    val groupSize = math.ceil(values.size.toDouble / Processors).toInt
+    val tasks  = values.grouped(groupSize).map(v => Future(foldMap(v)(func)))
+    Future.sequence(tasks).map(v => foldMap(v.toVector)(identity))
+  }
 
 }
--- src/test/scala/scalawithcats/exercises/chapter09/FoldMapSpec.scala
+++ src/test/scala/scalawithcats/exercises/chapter09/FoldMapSpec.scala
@@ -1,14 +1,20 @@
 package scalawithcats.exercises.chapter09
 
-import org.scalatest.{FlatSpec, Matchers}
 import cats.implicits._
+import org.scalatest.{AsyncFlatSpec, Matchers}
 
-class FoldMapSpec extends FlatSpec with Matchers {
+class FoldMapSpec extends AsyncFlatSpec with Matchers {
 
   behavior of "foldMap"
 
-  it should "apply a function to the input sequence and fold over the reuslt" in {
+  it should "apply a function to the input sequence and fold over the result" in {
     FoldMap.foldMap(Vector(1, 2, 3))(_ + 1) shouldBe 9
+  }
+
+  behavior of "parallelFoldMap"
+
+  it should "apply a function to the input sequence and fold over the result" in {
+    FoldMap.parallelFoldMap(Vector(1, 2, 3))(_ + 1) map { result => result shouldBe 9 }
   }
 
 }
