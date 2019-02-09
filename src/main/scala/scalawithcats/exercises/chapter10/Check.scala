9dda54569e125068713476d36c7edb15da694aae
Carwyn Ellis <carwynellis@gmail.com>
2019-02-09 18:08:19 +0000

    10.3 simplify apply implementation using mapN

--- src/main/scala/scalawithcats/exercises/chapter10/Check.scala
+++ src/main/scala/scalawithcats/exercises/chapter10/Check.scala
@@ -2,7 +2,6 @@ package scalawithcats.exercises.chapter10
 
 import cats._
 import cats.data.Validated
-import cats.data.Validated.{Invalid, Valid}
 import cats.implicits._
 
 trait Check[E, A] {
@@ -11,12 +10,7 @@ trait Check[E, A] {
 
   def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
     case Pure(f) => f(a)
-    case And(x, y) => (x(a), y(a)) match {
-      case (Invalid(errorX), Invalid(errorY)) => Invalid(errorX |+| errorY)
-      case (Invalid(errorX), Valid(_))        => Invalid(errorX)
-      case (Valid(_), Invalid(errorY))        => Invalid(errorY)
-      case (Valid(_), Valid(_))               => Valid(a)
-    }
+    case And(x, y) => (x(a), y(a)).mapN((_, _) => a)
   }
 
 }
