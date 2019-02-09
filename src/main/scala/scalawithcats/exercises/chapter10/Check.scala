2ac650be011a4eeb06737bbd87027e0aad7422b3
Carwyn Ellis <carwynellis@gmail.com>
2019-02-09 18:04:53 +0000

    10.3 reimplement check in terms of Validated

--- src/main/scala/scalawithcats/exercises/chapter10/Check.scala
+++ src/main/scala/scalawithcats/exercises/chapter10/Check.scala
@@ -1,24 +1,26 @@
 package scalawithcats.exercises.chapter10
 
 import cats._
+import cats.data.Validated
+import cats.data.Validated.{Invalid, Valid}
 import cats.implicits._
 
 trait Check[E, A] {
 
   def and(that: Check[E, A]): Check[E, A] = And(this, that)
 
-  def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] = this match {
+  def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
     case Pure(f) => f(a)
     case And(x, y) => (x(a), y(a)) match {
-      case (Left(errorX), Left(errorY)) => Left(errorX |+| errorY)
-      case (Left(errorX), Right(_))     => Left(errorX)
-      case (Right(_), Left(errorY))     => Left(errorY)
-      case (Right(_), Right(_))         => Right(a)
+      case (Invalid(errorX), Invalid(errorY)) => Invalid(errorX |+| errorY)
+      case (Invalid(errorX), Valid(_))        => Invalid(errorX)
+      case (Valid(_), Invalid(errorY))        => Invalid(errorY)
+      case (Valid(_), Valid(_))               => Valid(a)
     }
   }
 
 }
 
 // Following ADT style in case study.
-final case class Pure[E, A](f: A => Either[E, A]) extends Check[E, A]
+final case class Pure[E, A](f: A => Validated[E, A]) extends Check[E, A]
 final case class And[E, A](x: Check[E, A], y: Check[E, A]) extends Check[E, A]
