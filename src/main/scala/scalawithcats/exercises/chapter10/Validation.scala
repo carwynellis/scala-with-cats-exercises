f02a9a9352504c1aee3be582710ade24ec6923b2
Carwyn Ellis <carwynellis@gmail.com>
2019-02-20 20:08:14 +0000

    DEV initial email and username validators
    * email validator doesn't make use of all the existing predicates so
    needs to be revisited

--- src/main/scala/scalawithcats/exercises/chapter10/Validation.scala
+++ src/main/scala/scalawithcats/exercises/chapter10/Validation.scala
@@ -1,7 +1,7 @@
 package scalawithcats.exercises.chapter10
 
 import cats.data.NonEmptyList
-import scalawithcats.exercises.chapter10.Check.{AndThen, Pure}
+import scalawithcats.exercises.chapter10.Check.Pure
 
 object Validation {
 
@@ -29,6 +29,13 @@ object Validation {
     str => str.count(c => c == char) == 1
   )
 
-  val validateUsername = AndThen(Pure(longerThan(3)), Pure(alphanumeric))
+  val validateUsername = Pure(longerThan(3)) andThen Pure(alphanumeric)
 
+  // TODO - express in terms of existing predicates?
+  val validateEmailAddress = Pure(contains('@'))
+    .map(_.split('@'))
+    .andThen(Pure(Predicate.lift(error("Must contain characters before the @ sign"), a => a.headOption.forall(_.length > 0))))
+    .andThen(Pure(Predicate.lift(error("Must contain a . after the @ sign"), b => b.tail.headOption.forall(_.contains('.')))))
+    .andThen(Pure(Predicate.lift(error("Must contain at least 3 characters after the @ sign"), b => b.tail.headOption.forall(_.length > 3))))
+    .map(_.mkString("@"))
 }
--- src/test/scala/scalawithcats/exercises/chapter10/ValidationSpec.scala
+++ src/test/scala/scalawithcats/exercises/chapter10/ValidationSpec.scala
@@ -8,7 +8,7 @@ class ValidationSpec extends FlatSpec with Matchers {
 
   behavior of "validateUsername"
 
-  it should "accept a valid username string" in {
+  it should "accept a valid username" in {
     validateUsername("ValidUsername") shouldBe Valid("ValidUsername")
   }
 
@@ -18,6 +18,28 @@ class ValidationSpec extends FlatSpec with Matchers {
 
   it should "reject a username that does not contain only alphanumeric characters" in {
     validateUsername("FooBar!") shouldBe Invalid(error("Must be all alphanumeric characters"))
+  }
+
+  behavior of "validateEmailAddress"
+
+  it should "accept a valid email address" in {
+    validateEmailAddress("foo@example.com") shouldBe Valid("foo@example.com")
+  }
+
+  it should "reject an email address that does not contain an @ symbol" in {
+    validateEmailAddress("example.com") shouldBe Invalid(error("Must contain the character @"))
+  }
+
+  it should "reject an email address that contains no characters before the @ sign" in {
+    validateEmailAddress("@example.com") shouldBe Invalid(error("Must contain characters before the @ sign"))
+  }
+
+  it should "reject an email address that contains less than three characters after the @ sign" in {
+    validateEmailAddress("foo@e.") shouldBe Invalid(error("Must contain at least 3 characters after the @ sign"))
+  }
+
+  it should "reject an email address that does not contain a . in the string following the @ sign" in {
+    validateEmailAddress("foo@examplecom") shouldBe Invalid(error("Must contain a . after the @ sign"))
   }
 
 }
