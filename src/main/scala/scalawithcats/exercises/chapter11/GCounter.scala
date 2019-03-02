db8ed50df35af170ff6f0a46fa5f7a332070d9fa
Carwyn Ellis <carwynellis@gmail.com>
2019-03-02 17:48:53 +0000

    11.2.3 simplify merge implementation

--- src/main/scala/scalawithcats/exercises/chapter11/GCounter.scala
+++ src/main/scala/scalawithcats/exercises/chapter11/GCounter.scala
@@ -5,14 +5,11 @@ final case class GCounter(counters: Map[String, Int]) {
   def increment(machine: String, amount: Int): GCounter =
     GCounter(counters.updated(machine, counters.get(machine).fold(amount)(_ + amount)))
 
-  def merge(that: GCounter): GCounter = {
-    val merged = counters.keySet.union(that.counters.keySet).map { machine =>
-      val thisCount = counters.get(machine).fold(0)(identity)
-      val thatCount = that.counters.get(machine).fold(0)(identity)
-      if (thisCount > thatCount) machine -> thisCount else machine -> thatCount
+  def merge(that: GCounter): GCounter = GCounter(
+    counters.keySet.union(that.counters.keySet).map { machine =>
+      machine -> counters.getOrElse(machine, 0).max(that.counters.getOrElse(machine, 0))
     }.toMap
-    GCounter(merged)
-  }
+  )
 
   def total: Int = counters.values.sum
 
