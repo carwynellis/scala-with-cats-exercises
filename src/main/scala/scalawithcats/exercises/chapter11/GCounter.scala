ea5fe349e3fc03d8cee5b0b6c7110185d51e9845
Carwyn Ellis <carwynellis@gmail.com>
2019-03-09 17:58:23 +0000

    11.5 incorporate KeyValueStore into GCounter

--- src/main/scala/scalawithcats/exercises/chapter11/GCounter.scala
+++ src/main/scala/scalawithcats/exercises/chapter11/GCounter.scala
@@ -3,6 +3,7 @@ package scalawithcats.exercises.chapter11
 import cats.Monoid
 import cats.implicits._
 import scala.language.higherKinds
+import KeyValueStore.Syntax._
 
 trait GCounter[F[_,_],K, V] {
   def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
@@ -13,13 +14,13 @@ trait GCounter[F[_,_],K, V] {
 object GCounter {
   def apply[F[_,_], K, V](implicit counter: GCounter[F, K, V]) = counter
 
-  implicit def mapInstance[K,V] = new GCounter[Map, K, V] {
-
-    override def increment(f: Map[K, V])(k: K, v: V)(implicit m: Monoid[V]): Map[K, V] =
-      f.updated(k, v |+| f.getOrElse(k, m.empty))
-
-    override def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] = f1 |+| f2
-
-    override def total(f: Map[K, V])(implicit m: Monoid[V]): V = f.values.fold(m.empty)(_ |+| _)
-  }
+  implicit def gcounterInstance[F[_,_], K, V](implicit kvs: KeyValueStore[F], km: Monoid[F[K, V]]) =
+    new GCounter[F, K, V] {
+      def increment(f: F[K, V])(key: K, value: V)(implicit m: Monoid[V]): F[K, V] = {
+        val total = f.getOrElse(key, m.empty) |+| value
+        f.put(key, total)
+      }
+      def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V] = f1 |+| f2
+      def total(f: F[K, V])(implicit m: Monoid[V]): V = f.values.combineAll
+    }
 }
