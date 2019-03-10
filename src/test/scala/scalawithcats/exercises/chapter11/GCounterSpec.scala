7ebb28fbae269e7c6e1a04c5656fadf274506db9
Carwyn Ellis <carwynellis@gmail.com>
2019-03-10 18:04:11 +0000

    Resolve test failure by scoping implicits.
    We need the int instance to ensure increment |+| works as you would
    expect.
    
    We need the BoundedSemiLattice instance to handle the max behaviour via
    a |+| b. Unfortuantely since both are Monoid[Int] we cannot have both in
    scope at the same time.
    
    For now I've scoped the required implicits explicitly in the tests to
    ensure the correct implicit is available for the behaviour that we need.

--- src/test/scala/scalawithcats/exercises/chapter11/GCounterSpec.scala
+++ src/test/scala/scalawithcats/exercises/chapter11/GCounterSpec.scala
@@ -1,40 +1,60 @@
 package scalawithcats.exercises.chapter11
 
 import org.scalatest.{FlatSpec, Matchers}
-import cats.implicits._
+import GCounter._
+import cats.instances.map._
 
+// TODO - scoping the implicits provides the desired behaviour.
+//      - we cannot have the BoundedSemiLattice int instance in scope with the Monoid[Int] instance from cats since
+//        they are ambiguous.
+//      - this is pretty ugly so surely there is a better way?
 class GCounterSpec extends FlatSpec with Matchers {
 
-  val underTest = GCounter[Map, String, Int]
+  val underTest = {
+    import BoundedSemiLattice._
+    GCounter[Map, String, Int]
+  }
 
   behavior of "increment"
 
-  it should "increment the count for the specified machine" in {
-    underTest.increment(Map("A" -> 10))("A", 1) shouldBe Map("A" -> 11)
-  }
+  {
+    import cats.instances.int._
 
-  it should "add a new entry for a machine not present in counters" in {
-    underTest.increment(Map.empty)("A", 1) shouldBe Map("A" -> 1)
+    it should "increment the count for the specified machine" in {
+      underTest.increment(Map("A" -> 10))("A", 1) shouldBe Map("A" -> 11)
+    }
+
+    it should "add a new entry for a machine not present in counters" in {
+      underTest.increment(Map.empty)("A", 1) shouldBe Map("A" -> 1)
+    }
   }
 
   behavior of "merge"
 
-  it should "combine entries from two counters" in {
-    underTest.merge(Map("A" -> 1), Map("B" -> 2)) shouldBe Map("A" -> 1, "B" -> 2)
-  }
+  {
+    import BoundedSemiLattice._
 
-  it should "accept the higher value for a given machine value" in {
-    underTest.merge(Map("A" -> 1), Map("A" -> 10)) shouldBe Map("A" -> 10)
+    it should "combine entries from two counters" in {
+      underTest.merge(Map("A" -> 1), Map("B" -> 2)) shouldBe Map("A" -> 1, "B" -> 2)
+    }
+
+    it should "accept the higher value for a given machine value" in {
+      underTest.merge(Map("A" -> 1), Map("A" -> 10)) shouldBe Map("A" -> 10)
+    }
   }
 
   behavior of "total"
 
-  it should "return zero for an empty counters map" in {
-    underTest.total(Map.empty) shouldBe 0
-  }
+  {
+    import cats.instances.int._
 
-  it should "return the sum of all counters" in {
-    underTest.total(Map("A" -> 1, "B" -> 2)) shouldBe 3
+    it should "return zero for an empty counters map" in {
+      underTest.total(Map.empty) shouldBe 0
+    }
+
+    it should "return the sum of all counters" in {
+      underTest.total(Map("A" -> 1, "B" -> 2)) shouldBe 3
+    }
   }
 
 }
